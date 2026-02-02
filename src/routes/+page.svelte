8579f9980d69b41c68748143639ac2af86dfc1cd
parkss207 <parkss207@gmail.com>
2026-02-02 17:03:23 +0900

    SvelteKit 및 Cloudflare 어댑터 초기 설정 추가

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  // Simple welcome script
+</script>
+
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
